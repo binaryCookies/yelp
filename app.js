7ea829ad9c99d50fb0d08aa08c6ab0269cb45efb
Yanal <92484542+neom-yk@users.noreply.github.com>
2022-01-02 11:08:33 -0500

    End of Video 414 DELETE
    Added form and delete button to show page, used app.delete method in app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const Campground = require("./models/campground");
 
 //?Mongoose - created db name yelp-camp from path
 const mongoose = require("mongoose");
+//? VIDEO 413 npm i method-override (fake a put, patch or delete)
+const methodOverride = require("method-override");
 const campground = require("./models/campground.js");
 const { findById } = require("./models/campground");
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
@@ -17,6 +19,11 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log("DATABASE CONNECTED");
 });
+
+//? VIDEO 412 new.ejs - New and Create Submit Form
+app.use(express.urlencoded({ extended: true }));
+//?VIDEO 413 npm i method-override
+app.use(methodOverride("_method"));
 
 //? HOME PAGE ROUTE
 app.get("/", (req, res) => {
@@ -41,10 +48,46 @@ app.get("/campgrounds", async (req, res) => {
   res.render("campgrounds/index", { campgrounds });
 });
 
-//? VIDEO 410 Campground Show - use id to lookup corresponding campground
+//? Video 412 new.ejs, Create New campground - Form
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+//? VIDEO 412 new.ejs - New and Create Submit Form
+//? (must parse data urlendoded...)
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  //  res.send(req.body); // testing post route
+  await campground.save();
+  console.dir(req);
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+//? VIDEO 410 Campground Show Page - use id to lookup corresponding campground
 app.get("/campgrounds/:id", async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render("campgrounds/show", { campground });
+});
+
+//? VIDEO 413 - Route Edit.ejs: edit and update
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campground });
+});
+//? VIDEO 413 - Route Edit.ejs: edit and update
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  res.redirect(`/campgrounds/${campground._id}`);
+  // res.send("it worked");
+});
+
+app.delete(`/campgrounds/:id`, async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndDelete(id);
+  res.redirect("/campgrounds");
 });
 
 app.listen(3000, (req, res) => {
