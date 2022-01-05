3fa2354f67e32305950296dee36b5a3d409e3627
Yanal <92484542+neom-yk@users.noreply.github.com>
2022-01-05 10:48:12 -0500

    End of Video 427 Adding Images
     Updated model to accept price and description
    Updated seeds file DB function with properties description and image
    Updated sjow.ejs to have img src anddisplay campground description

--- seeds/index.js
+++ seeds/index.js
@@ -19,10 +19,16 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city},  ${cities[random1000].state}`,
       //? VIDEO 409 testing data
       title: `${sample(descriptors)} ${sample(places)}`,
+      //? VIDEO 427 Adding Images (modified model to match)
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque velit, nemo maiores nesciunt voluptas perferendis cupiditate quia facere! Quas modi ullam rem quos enim voluptates? Natus assumenda alias architecto culpa.",
+      price,
     });
     await camp.save();
   }
