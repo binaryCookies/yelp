546a29d80956a31409611ce0b9610b90a73d9231
Yanal <92484542+neom-yk@users.noreply.github.com>
2022-02-27 13:06:29 -0500

    VIDEO 488 SERVING STATIC ASSETS - CREATED PUBLIC FOLDER
    Created public directory and adde sub-folder javascripts and stylesheets
    Moved bootstrap validation script to validateForms.js in the public -> javascripts directory
    Public directory can be use for scripts, logos, images etc.
    Added to app.js file path connecting public directory  app.use(express.static(path.join(__dirname, "public")));

---
+++ public/javascripts/validateForms.js
@@ -0,0 +1,29 @@
+//* VIDEO 488 SERVING STATIC ASSETS
+//* Moved bootstrap function from boilerplate.ejs - makes sense to group in public directory because its client side form validation
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      const forms = document.getElementsByClassName("validated-form");
+      // Loop over them and prevent submission
+      const validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
