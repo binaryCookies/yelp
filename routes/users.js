14944b89009dd90b520627d0a607ab76df09557b
Yanal <92484542+neom-yk@users.noreply.github.com>
2022-03-06 14:16:01 -0500

    VIDEO 513 REGISTERING ROUTE LOGIC - updating route logic
    In user.js: added a destructured variable of what we want from req.body
    Created new user and passed email and username
    Then passed email and username to the new User
    Called new User.register - takes instance of new User email and password (password hashed with passport-local-mongoose)
    Flashed success message
    
    Applied error handler catchAsync
    Wrapped route in try and catch to flash unique error message
    Added flash error message for the try/catch
    Redirect to register if error caught otherwise redirect to /campgrounds

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,34 @@
 
 const express = require("express");
 const router = express.Router();
+// VIDEO 513
+const catchAsync = require("../utils/catchAsync");
 const User = require("../models/user");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
-router.post("/register", async (req, res) => {
-  res.send(req.body);
-});
+
+// CREATE USER VIDEO 513
+router.post(
+  "/register",
+  catchAsync(async (req, res) => {
+    try {
+      // Destructure what we want from req.body
+      const { email, username, password } = req.body;
+      // Pass email and username to new User
+      const user = new User({ email, username });
+      // Call User.register() takes instance of new user and should also hash the password (because of passport-local-mongoose)
+      const registeredUser = await User.register(user, password);
+      req.flash("success", "Welcome to Yelp Camp!");
+      res.redirect("/campgrounds");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("register");
+    }
+    // console.log(registeredUser);
+    req.flash("success", "Welcome to Yelp Camp!");
+    res.redirect("/campgrounds");
+  })
+);
 module.exports = router;
