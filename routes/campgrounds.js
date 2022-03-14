b949ef01ee1fde0875080fbf5786f8ee14d13dc6
Yanal <92484542+neom-yk@users.noreply.github.com>
2022-03-14 10:52:49 -0400

    531-532 THE MULTER MIDDLEWAR - setup image parsing middleware for local storage
    Modified app.js notes only, routes/campgrounds.js, new.ejs
    Started process to parse images and eventually send to Cloudinary for storing the images
    Added new input form form images - connected  form to where it submits to -> post route -> tested route w/ res.send req.body
    Added middleware Multer - npm i multer
    Input form added attribute enctype="multipart/form-data" (parsing images capable as opposed to express.urlencoded)
    Image form input added multiple attribute for multiple images, routes/campgrounds.js added before (req,res) upload.array("image") - array is for multiple images - single would be for single image
    Input form: name="image" should match upload.array("image") - image in both or whatever name in one used in the other

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -14,7 +14,7 @@
  **   1. Find campground -> 2. Check if we can update
  ** VIDEO 523 AUTHORIZATION MIDDLEWARE - isAuthor middleware applied to edit, delete, route   (deleted old permission block for isAuthor middleware ), required { isLoggedIn, isAuthor, validateCampground }, exported modules from middleware file
  ** Moved validateReviews to middleware file from reviews.js file
- ** VIDEO 525 MORE REVIEWS AUTHORIZATION - campground.js populate author of each review, added reviews by in show.ejs under reviews section. (username access possible by review loop of each campground), hide delete button, protect delete route
+ ** VIDEO 525 MORE REVIEWS AUTHORIZATION - campground.js populate author of each review, added reviews by in show.ejs under reviews section. (username access possible by review loop of each campground), hide delete button, protect delete route,
  */
 
 const express = require("express");
@@ -23,19 +23,28 @@ const catchAsync = require("../utils/catchAsync");
 // const { campgroundSchema } = require("../schemas.js");  moved to middleware file
 
 const Campground = require("../models/campground");
-//* VIDEO 515 isLoggedIn
+//VIDEO 515 isLoggedIn
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 const ExpressError = require("../utils/ExpressError");
 const campgrounds = require("../controllers/campgrounds");
+//VIDEO 532 THE MULTER MIDDLEWARE
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" }); //destination and path
 
 //*VIDEO 528 A FANCY WAY TO RESTRUCTURE ROUTES - docs: express router.route
 //? All Campgrounds - INDEX Route
-router.route("/").get(catchAsync(campgrounds.index)).post(
-  isLoggedIn,
-  validateCampground,
-  catchAsync(campgrounds.createCampground)
-  // added validateCampground video 447, isLoggedIn Video 515
-);
+router
+  .route("/")
+  .get(catchAsync(campgrounds.index))
+  // .post(
+  //   isLoggedIn,
+  //   validateCampground, //added validateCampground video 447, isLoggedIn Video 515
+  //   catchAsync(campgrounds.createCampground));//
+  .post(upload.array("image"), (req, res) => {
+    //VIDEO 532 MULTER - upload.single('image') - image is the form field name
+    console.log(req.body, req.files); // pluaralized file when setting changed from single to array
+  });
+
 //? Video 412 new.ejs, Create New campground - Form. MUST HAVE NEW ROUTE ABOVE ROUTE ID
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 router
