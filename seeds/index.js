27c12dcd8c094e350bb0b42fe41e09bded26a02f
Yanal <92484542+neom-yk@users.noreply.github.com>
2022-03-23 08:07:53 -0400

    VIDEO 554 RESEEDING OUR DATABASE - reseed each city with latitude and longitude
    Changed seeds/index.js to reseed with latitude and longitude provided from the array in the cities.js file
    Coordinates updated to import latitude and longitude and combine geolocation into single array

--- seeds/index.js
+++ seeds/index.js
@@ -38,7 +38,10 @@ const seedDB = async () => {
       price,
       geometry: {
         type: "Point",
-        coordinates: [-73.610364, 45.497216],
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
       },
     });
     await camp.save();
